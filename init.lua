e38f11c7118b89723e40ff269e7a6d55cd8c1856
hui00 <kevinbrinkschulte@hotmail.de>
2023-01-03 22:34:56 +0100

    bufferline

--- init.lua
+++ init.lua
@@ -26,26 +26,7 @@ vim.api.nvim_set_keymap("n", "<Leader><Leader>",
 
 require("bufferline").setup{
   options = {
-    numbers = "none",
-    number_style = "",
-    mappings = true,
-    buffer_close_icon = "",
-    modified_icon = "",
-    close_icon = "",
-    left_trunc_marker = "",
-    right_trunc_marker = "",
-    max_name_length = 18,
-    max_prefix_length = 15,
-    tab_size = 18,
-    enforce_regular_tabs = false,
-    show_buffer_close_icons = true,
-    show_close_icon = true,
-    show_tab_indicators = true,
-    persist_buffer_sort = true,
-    separator_style = "thin",
-    always_show_bufferline = true,
-    sort_by = "directory",
-    diagnostics = "nvim_lsp"
+    -- diagnostics = "nvim_lsp"
   }
 }
 
